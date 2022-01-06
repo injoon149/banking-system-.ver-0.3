27688e0c7a13dcfcd2b735cb8e020d382dfba94f
choi injoon <dlswns8679@naver.com>
2022-01-06 18:41:07 +0900

    초기 커밋.

---
+++ banking system .ver 0.3/banking system .ver 0.3.cpp
@@ -0,0 +1,223 @@
+﻿/*
+* Banking system ver 0.3
+* 작성자 : 최인준
+* 내용: AccountHandler라는 이름의 컨트롤 클래스 정의(하는 일 분할)
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int NAME_LEN = 20;
+
+enum { MAKE = 1, DEPOSIT, WITHDRAW, INQUIRE, EXIT }; // 1,2,3,4,5의 이름 지정
+
+/*
+* 클래스 이름: Account
+* 클래스 유형: entity 클래스
+*/
+
+class Account
+{
+private:
+	int accID; // 계좌번호
+	int balance; // 잔액
+	char* cusName; // 고객이름
+
+public:
+	Account(int ID, int money, char* name);
+	Account(const Account& ref);
+
+	int GetAccID() const;
+	void Deposit(int money);
+	int WithDraw(int money);
+	void ShowAccInfo() const;
+	~Account();
+};
+
+Account::Account(int ID, int money, char* name) : accID(ID), balance(money)
+{
+	cusName = new char[strlen(name) + 1];
+	strcpy_s(cusName, strlen(name) + 1, name);
+}
+
+Account::Account(const Account& ref) : accID(ref.accID), balance(ref.balance)
+{
+	cusName = new char[strlen(ref.cusName) + 1];
+	strcpy_s(cusName, strlen(ref.cusName) + 1, ref.cusName);
+}
+
+int Account::GetAccID() const
+{
+	return accID;
+}
+
+void Account::Deposit(int money)
+{
+	balance += money;
+}
+
+int Account::WithDraw(int money)
+{
+	if (balance < money) return 0;
+
+	balance -= money;
+	return money;
+}
+
+void Account::ShowAccInfo() const
+{
+	cout << "계좌ID: " << accID << endl;
+	cout << "이름: " << cusName << endl;
+	cout << "잔액: " << balance << endl;
+}
+
+Account::~Account()
+{
+	delete[]cusName;
+}
+
+/*
+* 클래스 이름: AccountHandler
+* 클래스 유형: 컨트롤 클래스
+*/
+
+class AccountHandler
+{
+private:
+	Account* accArr[100];
+	int accNum;
+public:
+	AccountHandler();
+	void ShowMenu(void) const;
+	void MakeAccount(void);
+	void DepositMoney(void);
+	void WithdrawMoney(void);
+	void ShowAllAccInfo(void) const;
+	~AccountHandler();
+
+};
+
+void AccountHandler::ShowMenu(void) const
+{
+	cout << "-----Menu-----" << endl;
+	cout << "1.계좌개설" << endl;
+	cout << "2. 입금" << endl;
+	cout << "3. 출금" << endl;
+	cout << "4. 계좌정보 전체 출력" << endl;
+	cout << "5. 프로그램 종료" << endl;
+
+}
+
+void AccountHandler::MakeAccount(void)
+{
+	int id;
+	char name[NAME_LEN];
+	int balance;
+
+	cout << "[계좌개설]" << endl;
+	cout << "계좌ID: "; cin >> id;
+	cout << "이름: "; cin >> name;
+	cout << "입금액: "; cin >> balance;
+	cout << endl;
+
+	accArr[accNum++] = new Account(id, balance, name);
+}
+
+void AccountHandler::DepositMoney(void)
+{
+	int money;
+	int id;
+	cout << "[입금]" << endl;
+	cout << "계좌ID:"; cin >> id;
+	cout << "입금액: "; cin >> money;
+
+	for (int i = 0; i < accNum; i++)
+	{
+		if (accArr[i]->GetAccID() == id)
+		{
+			accArr[i]->Deposit(money);
+			cout << "입금완료" << endl << endl;
+			return;
+		}
+	}
+	cout << "유효하지 않은 ID입니다." << endl << endl;
+}
+
+void AccountHandler::WithdrawMoney(void)
+{
+	int money;
+	int id;
+	cout << "[출금]" << endl;
+	cout << "계좌ID: "; cin >> id;
+	cout << "출금액: "; cin >> money;
+
+	for (int i = 0; i < accNum; i++)
+	{
+		if (accArr[i]->GetAccID() == id)
+		{
+			if (accArr[i]->WithDraw(money) == 0)
+			{
+				cout << "잔액부족" << endl << endl;
+				return;
+			}
+
+			cout << "출금완료" << endl << endl;
+			return;
+		}
+	}
+	cout << "유효하지 않은 ID입니다." << endl << endl;
+}
+
+AccountHandler::AccountHandler() : accNum(0)
+{}
+
+void AccountHandler::ShowAllAccInfo(void) const
+{
+	for (int i = 0; i < accNum; i++)
+	{
+		accArr[i]->ShowAccInfo();
+		cout << endl;
+	}
+}
+
+AccountHandler::~AccountHandler()
+{
+	for (int i = 0; i < accNum; i++)
+		delete accArr[i];
+}
+
+int main(void)
+{
+	AccountHandler manager;
+	int choice;
+
+	while (1)
+	{
+		manager.ShowMenu();
+		cout << "선택: "; cin >> choice;
+		cout << endl;
+
+		switch (choice)
+		{
+		case MAKE:
+			manager.MakeAccount();
+			break;
+		case DEPOSIT:
+			manager.DepositMoney();
+			break;
+		case WITHDRAW:
+			manager.WithdrawMoney();
+			break;
+		case INQUIRE:
+			manager.ShowAllAccInfo();
+			break;
+		case EXIT:
+			return 0;
+		default:
+			cout << "Illegal selection.." << endl;
+		}
+
+	}
+	return 0;
+}
